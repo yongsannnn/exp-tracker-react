8bc8679acea73501bb2c1566279f67bc7a5fc610
yongsannnn <yong_s@live.com>
2021-05-13 10:36:35 +0000

    added login and logout button with conditional rendering for header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import LoginContext from "./LoginContext"
+import { Link } from "react-router-dom"
 
 
 export default function Header() {
+    let context = useContext(LoginContext)
+
+    const logoutUser = async () => {
+        localStorage.clear()
+        context.changeLogin()
+    }
+    
     return (
         <React.Fragment>
             <h1>Header</h1>
+            <Link style={{
+                display: context.checkLogin() === true ? "block" : "none"
+            }} to="/" onClick={logoutUser}>Log Out</Link>
+            <Link style={{
+                display: context.checkLogin() === false ? "block" : "none"
+            }} to="/login">Log In</Link>
         </React.Fragment>
 
     )
